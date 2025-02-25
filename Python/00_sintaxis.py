83729022a88fbe2507767e0079ea8df4500ca936
Francisco Damián Gonzalez Rivera <logistic.proyect234@gmail.com>
2025-02-24 21:44:06 -0600

    Inicio del reto de 101 ejercicios

---
+++ Python/00_sintaxis.py
@@ -0,0 +1,24 @@
+#https://www.python.org/
+
+# Comentario en un a línea para pyhton
+
+"""
+Esto tambien es 
+un comentario 
+en varias líneas  
+"""
+
+#Para declarar una variable en python, unicamente tenemos que hacer uso del snake_case y asignales el tupi de dato que corresponda
+
+my_variable = "Mi variable"
+my_variable = "Mi nueva variable"
+
+#Tipós de datos bi
+#Entero (int), decimales (float), booleano o "Cierto/Falso" (bool) y cadenas de texto (str)
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Podemos hacer uso de las dobles comillas"
+my_string = 'Tambien de comillas simples'
